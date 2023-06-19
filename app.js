e78a03c52dac5c0097c421f009dfad7deaabe760
PuneetMahendroo <104011848+PuneetMahendroo@users.noreply.github.com>
2023-06-19 21:12:14 +0530

    aditya bhaiya ki jai ho

---
+++ app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+    "h1",
+    {
+        id:"title",
+    },
+    "Heading1"
+);
+// <h1 id="title">Heading 1</h1>
+
+const heading2 = React.createElement(
+    "h2",
+    { 
+        id:"title",
+        hello:"world",
+    }, 
+    "Heading2"
+);
+
+const container = React.createElement("div",{ id:"container",}, [heading1,heading2]);
+console.log(heading1);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
